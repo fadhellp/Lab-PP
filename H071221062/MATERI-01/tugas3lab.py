4903ccdc997817d7b61151919a82094a099564fd
ilyfauzan <ahmdfauzanrr@gmail.com>
2022-09-23 15:36:01 +0800

    kumpul tugas 1

---
+++ H071221062/MATERI-01/tugas3lab.py
@@ -0,0 +1,17 @@
+print ("NAMA\t : AHMAD FAUZHAN RAMADHAN B")
+print ("NIM\t : H071221062")
+print ("Grup\t : A")
+
+import math
+h = int(input("Tinggi menara ="))
+a = int(input("Masukkan sudut elevasi a ="))
+b = int(input("Masukkan sudut elevasi b ="))
+
+a = (a)*math.pi/180
+b = (b)*math.pi/180
+panjang_a = math.tan(a)*(h)
+panjang_b = math.tan(b)*(h)
+panjang_kapal = panjang_a - panjang_b
+print(f"Panjang kapal = {panjang_kapal:.1f} m") #
+
+print ("=====ARIGATOOOO=====")
