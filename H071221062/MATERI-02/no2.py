572359bdd9d8808eb5f747bcd4c924bc2ae19af9
ilyfauzan <ahmdfauzanrr@gmail.com>
2022-09-29 11:56:56 +0800

    kumpul tugas 2

---
+++ H071221062/MATERI-02/no2.py
@@ -0,0 +1,8 @@
+golongan = int(input("Masukkan golongan :"))
+daya = int(input("Masukkan daya :"))
+pemakaian = int(input("Masukkan pemakaian :"))
+
+if golongan RI == > daya 900 and daya 1300 and 2200 < :
+    tarif = 1.352
+
+elif golongan R
