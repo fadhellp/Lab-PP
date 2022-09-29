b63cab4bb0ff9e136260757b1d4be1e5914f562a
ilyfauzan <114056316+ilyfauzan@users.noreply.github.com>
2022-09-29 12:05:41 +0800

    Delete no2.py

--- H071221062/MATERI-02/no2.py
+++
@@ -1,8 +0,0 @@
-golongan = int(input("Masukkan golongan :"))
-daya = int(input("Masukkan daya :"))
-pemakaian = int(input("Masukkan pemakaian :"))
-
-if golongan RI == > daya 900 and daya 1300 and 2200 < :
-    tarif = 1.352
-
-elif golongan R
