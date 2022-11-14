c27c83ee8222efd92fe4882ff4581fed9a1fbaa1
ilyfauzan <ahmdfauzanrr@gmail.com>
2022-11-14 17:55:08 +0800

    KUMPUL TUGAS TERBARU

---
+++ H071221062/MATERI-04/no2.py
@@ -0,0 +1,13 @@
+def isiFPB(a,b): #20,30 ->30,10 ->10,0
+    if (b==0):
+        return a
+    else:
+        return isiFPB(b,a%b) #30, 20 % 30 = 10 -> 10,30 % 10= 0
+
+input_a = int(input())
+input_b = int(input())
+
+print('FPB( %s,%s)='%(input_a,input_b),isiFPB(input_a,input_b))
+
+if input_a == 0 or input_b == 0:
+    print('salahhhh')
